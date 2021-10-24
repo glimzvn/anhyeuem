f77ab5efce08e872ce7a8b199851ff02974e9334
nhatnamtk <82993726+nhatnamtk@users.noreply.github.com>
2021-10-24 13:23:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yourDate = new Date("2016-10-30T02:10:26"),
+const yourDate = new Date("2021-8-07T20:10:26"),
 music = ['ido', 'noinaycoanh', 'nguoiamphu'];
 
 document.addEventListener('DOMContentLoaded', function(){
